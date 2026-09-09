4fe28168af2d7c89d552389cd1d3099357596e12
AMAL DAS <office.amaldas@gmail.com>
2026-09-09 17:07:28 +0530

    Update Google Analytics 4 Tracking ID to correct value

--- worker/src/config-analytics.js
+++ worker/src/config-analytics.js
@@ -3,18 +3,18 @@
 // ==========================================
 
 export default {
-  // ----- Google Analytics 4 (GA4) Tracking ID -----
-  gaTrackingId: "G-EQGWN6G3DH",
+  // ----- Google Analytics 4 (GA4) Tracking ID (সঠিক আইডি) -----
+  gaTrackingId: "G-EQGW6G3DH",
 
   // ----- অতিরিক্ত হেড স্ক্রিপ্ট (GA4 gtag.js) -----
   extraHeadScripts: `
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQGWN6G3DH"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQGW6G3DH"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', 'G-EQGWN6G3DH');
+      gtag('config', 'G-EQGW6G3DH');
     </script>
   `
 };
